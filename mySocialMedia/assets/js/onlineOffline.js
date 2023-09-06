e7d06d4d7c26c0a2a781d24e7ee09e433db682ef
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-09-06 13:58:34 -0300

    feat: navigator.onLine

---
+++ mySocialMedia/assets/js/onlineOffline.js
@@ -0,0 +1,36 @@
+let statusElement = document.getElementById('status');
+var target = document.getElementById('target');
+
+function updateStatusFromLocalStorage() {
+    let state = localStorage.getItem('connectionStatus');
+
+    if (state) {
+        // Remover ambas as classes antes de adicionar uma nova
+        statusElement.classList.remove('online', 'offline');
+
+        if (state === 'online') {
+            statusElement.textContent = 'online';
+            statusElement.classList.add("online");
+        } else {
+            statusElement.textContent = 'offline';
+            statusElement.classList.add('offline');
+        }
+    }
+}
+
+function handleStateChange(event) {
+    var newState = document.createElement('p');
+    var state = event.type === 'online' ? 'online' : 'offline';
+
+    // Armazene o status no localStorage apenas se ele mudar
+    if (state !== localStorage.getItem('connectionStatus')) {
+        localStorage.setItem('connectionStatus', state);
+        window.location.reload();
+    }
+}
+
+// Atualize o elemento de status na carga da página
+updateStatusFromLocalStorage();
+
+window.addEventListener('online', handleStateChange);
+window.addEventListener('offline', handleStateChange);
